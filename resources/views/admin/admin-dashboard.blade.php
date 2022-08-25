4a09b1fea5630c7ff8f629c60dc44597647e86fd
Thuta Min Thway <thutaminthway@Thutas-MacBook-Air.local>
2022-08-25 14:04:33 +0630

    admin dashboard fixed

--- resources/views/admin/admin-dashboard.blade.php
+++ resources/views/admin/admin-dashboard.blade.php
@@ -6,7 +6,7 @@
 		  <!-- Main content -->
 		  <section class="content">
 			  <div class="row">
-				  <div class="col-xl-2 col-6">
+				  <div class="col-xl-4 col-6">
 					  <div class="box overflow-hidden pull-up">
 						  <div class="box-body">							
 							  <div class="icon bg-primary-light rounded w-60 h-60">
@@ -19,7 +19,7 @@
 						  </div>
 					  </div>
 				  </div>
-				  <div class="col-xl-2 col-6">
+				  <div class="col-xl-4 col-6">
 					  <div class="box overflow-hidden pull-up">
 						  <div class="box-body">							
 							  <div class="icon bg-warning-light rounded w-60 h-60">
@@ -32,7 +32,7 @@
 						  </div>
 					  </div>
 				  </div>
-				  <div class="col-xl-2 col-6">
+				  <div class="col-xl-4 col-6">
 					  <div class="box overflow-hidden pull-up">
 						  <div class="box-body">							
 							  <div class="icon bg-info-light rounded w-60 h-60">
@@ -41,45 +41,6 @@
 							  <div>
 								  <p class="text-mute mt-20 mb-0 font-size-16">Sales Lost</p>
 								  <h3 class="text-white mb-0 font-weight-500">$1,250 <small class="text-danger"><i class="fa fa-caret-down"></i> -0.5%</small></h3>
-							  </div>
-						  </div>
-					  </div>
-				  </div>
-				  <div class="col-xl-2 col-6">
-					  <div class="box overflow-hidden pull-up">
-						  <div class="box-body">							
-							  <div class="icon bg-danger-light rounded w-60 h-60">
-								  <i class="text-danger mr-0 font-size-24 mdi mdi-phone-incoming"></i>
-							  </div>
-							  <div>
-								  <p class="text-mute mt-20 mb-0 font-size-16">Inbound Call</p>
-								  <h3 class="text-white mb-0 font-weight-500">1,460 <small class="text-danger"><i class="fa fa-caret-up"></i> -1.5%</small></h3>
-							  </div>
-						  </div>
-					  </div>
-				  </div>
-				  <div class="col-xl-2 col-6">
-					  <div class="box overflow-hidden pull-up">
-						  <div class="box-body">							
-							  <div class="icon bg-success-light rounded w-60 h-60">
-								  <i class="text-success mr-0 font-size-24 mdi mdi-phone-outgoing"></i>
-							  </div>
-							  <div>
-								  <p class="text-mute mt-20 mb-0 font-size-16">Outbound Call</p>
-								  <h3 class="text-white mb-0 font-weight-500">1,700 <small class="text-success"><i class="fa fa-caret-up"></i> +0.5%</small></h3>
-							  </div>
-						  </div>
-					  </div>
-				  </div>
-				  <div class="col-xl-2 col-6">
-					  <div class="box overflow-hidden pull-up">
-						  <div class="box-body">							
-							  <div class="icon bg-light rounded w-60 h-60">
-								  <i class="text-white mr-0 font-size-24 mdi mdi-chart-line"></i>
-							  </div>
-							  <div>
-								  <p class="text-mute mt-20 mb-0 font-size-16">Total Revune</p>
-								  <h3 class="text-white mb-0 font-weight-500">$4,500k <small class="text-success"><i class="fa fa-caret-up"></i> +2.5%</small></h3>
 							  </div>
 						  </div>
 					  </div>
